a94b71e8f1e6956ab9c65c0f246cca1dca9b73d6
dev7083 <113914000+Dev7083@users.noreply.github.com>
2024-09-11 22:01:45 +0530

    Req, Headers, URL Query, Cookie

--- my-app-2/app/api/products/route.js
+++ my-app-2/app/api/products/route.js
@@ -1,6 +1,24 @@
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
+    // console.log(req);
+
+    // Headers
+    const requestHeaders = new Headers(req.headers)
+    // console.log(requestHeaders);
+
+    // URL QUery Parameter
+    const { searchParams } = new URL(req.url)
+    // console.log(searchParams);
+    // console.log(searchParams);
+
+
+
+    // Cookie
+
+    const cook1 = req.cookies
+    console.log("Cookie1:", cook1);
+
     return NextResponse.json({
         "msg": "Hello NEXTJS API"
     })
